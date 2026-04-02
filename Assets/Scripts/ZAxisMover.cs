8f44f759bd1f08aff0abbb364ced5ad1a44141f6
wndmsrla0623-lu <wndmsrla0623@gmail.com>
2026-04-02 12:16:33 +0900

    [Feat] 유니티 프리팹 활용

---
+++ Assets/Scripts/ZAxisMover.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class ZAxisMover : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float timer = 5.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, speed * Time.deltaTime);
+
+        timer -= Time.deltaTime;
+
+        if (timer < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
